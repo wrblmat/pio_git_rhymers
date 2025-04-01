057a8687fb9aa5e2cd0eab62f951927961a69224
Wrobel251112 <251112@edu.p.lodz.pl>
2025-04-02 00:27:05 +0200

    10. Wygenerowanie gettera dla pola total

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -7,6 +7,10 @@ public class defaultCountingOutRhymer {
     private static final int MAX_ARRAY_SIZE = 12;
     private final int[] numbers = new int[MAX_ARRAY_SIZE];
 
+    public int getTotal() {
+        return total;
+    }
+
     private int total = EMPTY;
 
     public void countIn(int in) {
